cccc556ae635b6c9fbc81b1229251f1689094163
Hema Veeraragavathatham <hema.ragavan@gmail.com>
2024-11-12 14:46:38 -0800

    Updated firebase config file

--- config/firebase.js
+++ config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGE5mDroGJt5YDoM6g9IoBea9AudW-EL8",
-  authDomain: "snugglivet.firebaseapp.com",
-  projectId: "snugglivet",
-  storageBucket: "snugglivet.firebasestorage.app",
-  messagingSenderId: "355370673452",
-  appId: "1:355370673452:web:71721d9b0dd801cc40f889",
-  measurementId: "G-FB5D0RJJR5",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
