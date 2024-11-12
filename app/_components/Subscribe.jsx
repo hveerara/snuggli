d875aa7db6abfbf1831961268b4852e7c2e4aeb9
Hema Veeraragavathatham <hema.ragavan@gmail.com>
2024-11-12 15:10:16 -0800

    Additional style changes

--- app/_components/Subscribe.jsx
+++ app/_components/Subscribe.jsx
@@ -55,12 +55,12 @@ function Subscribe() {
     }
   };
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-screen-xl px-8 sm:px-4 sm:py-6 xs:px-4 xs:py-6 ">
       <h1 className="text-center text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold sm:text-3xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
         Let's Talk!
       </h1>
 
-      <h2 className="mx-auto mt-10 mb-10 max-w-screen-xl text-gray-500 text-xl md:text-2xl lg:text-2xl xl:text-3xl">
+      <h2 className="mx-auto mt-10 mb-10 xs:mt-7 xs:mb-7 sm:mt-7 sm:mb-7 max-w-screen-xl text-gray-500 text-xl md:text-2xl lg:text-2xl xl:text-3xl">
         Want to manage your practice more efficiently using AI-enabled
         technology?
       </h2>
@@ -84,7 +84,7 @@ function Subscribe() {
         )}
         <form
           onSubmit={handleSubmit}
-          className="mb-0 space-y-4 rounded-lg pt-0 sm:p-6 lg:p-8"
+          className="mb-0 space-y-4 pt-0 sm:p-6 lg:p-8"
           autoComplete="off"
         >
           <div className="px-3 py-3">
